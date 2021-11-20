c7c5343edc94d34260923c46a6e2dd9afd1705dd
chuvikovd <chuvikovd@gmail.com>
2021-11-20 19:25:22 +0200

    add title to challenge

--- src/pages/SoapChallenge.tsx
+++ src/pages/SoapChallenge.tsx
@@ -52,6 +52,10 @@ export const SoapChallenge = () => {
     return <>
         <Container maxWidth="xl">
             <Grid container spacing={2} justifyContent='center'>
+                <Grid item xs={12} sm={6} md={3} sx={{ mt: 2 }}>
+                    <Typography variant="h3" gutterBottom>Challenge</Typography>
+                </Grid>
+                <Grid item xs={12} sx={{ p: '0 !important' }} />
                 <Grid item xs={12} sm={6} md={3}>
                     <Card>
                     <CardHeader title="🧼 Don't drop it!" subheader="Soap your hands for 20 seconds" />
