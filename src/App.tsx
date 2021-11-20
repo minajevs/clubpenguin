742eb8ba4e1a04c6205f189684d4340b7a20cc8a
chuvikovd <chuvikovd@gmail.com>
2021-11-20 23:05:55 +0200

    remove unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-// routes
 import Router from './routes';
-// theme
-//@ts-ignore
 import ThemeConfig from './theme';
-//@ts-ignore
 import GlobalStyles from './theme/globalStyles';
-// components
-//@ts-ignore
 import ScrollToTop from './components/ScrollToTop';
-//@ts-ignore
-import { BaseOptionChartStyle } from './components/charts/BaseOptionChart';
-
-// ----------------------------------------------------------------------
 
 export default function App() {
   return (
     <ThemeConfig>
       <ScrollToTop />
       <GlobalStyles />
-      <BaseOptionChartStyle />
       <Router />
     </ThemeConfig>
   );
